12cb268c733b3d8c4cd038b84cbf28da72dd8cd1
rhdzmota <rhdzmota@gmail.com>
2019-08-04 01:48:29 -0500

    Update build script with submodule management

--- build.sh
+++ build.sh
@@ -1,16 +1,41 @@
 #!/usr/bin/env bash
 
+# Global variables
+date_string="$(date +%F)"
+commit_message_submodule="${date_string} Automatic update!"
+commit_message_main="${date_string} Automatic submodule tracking!"
+
 fix-urls-in-file () {
   local file="${1}"
   sed -i -e 's/\/http/http/g' "${file}"
 }
 
+exit-message () {
+  local message="${1}"
+  echo "${message}"
+  exit
+}
+
+git-submodule-management () {
+  cd ./docs/ || exit-message "[ERROR] Failed to change directory;"
+  echo "[INFO] Submodule add/commit/push changes;"
+  git commit -a -m "${commit_message_submodule}"
+  git push origin master
+  cd - || exit-message "[ERROR] Failed to return to main directory"
+  echo "[INFO] Main project submodule tracking (commit only);"
+  git add docs
+  git commit -m "${commit_message_main}"
+}
+
 # Build site using Academic Hugo
-echo "Building site with Academic Hugo;"
+echo "[INFO] Building site with Academic Hugo;"
 hugo
 
 # Fix URLs if needed
 while read -r file; do
-  echo "Analysing file (${file});"
+  echo "[INFO] Analysing file (${file});"
   fix-urls-in-file "${file}"
 done < <(find docs -name '*.html')
+
+# Add/commit submodule changes
+git-submodule-management
